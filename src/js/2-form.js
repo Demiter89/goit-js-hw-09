943508b5d40603da7af7abca012a92d097eef19a
Maksym Shevchuk <maksymshevchuk89@gmai.com>
2025-01-03 17:17:27 +0100

    add index file

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,51 +1,51 @@
 const feedbackFormEl = document.querySelector('.js-feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
 
-let formData = {
-  email: '',
-  message: '',
-};
+let formData = { email: '', message: '' };
 
 (() => {
   try {
-    const storageData = JSON.parse(localStorage.getItem('feedback-form-state'));
+    const storageData = JSON.parse(localStorage.getItem(STORAGE_KEY));
 
-    if (storageData === null) {
-      return;
-    }
+    if (!storageData) return;
 
     formData = storageData;
 
-    for (const key in storageData) {
+    Object.keys(storageData).forEach(key => {
       if (feedbackFormEl.elements[key]) {
         feedbackFormEl.elements[key].value = storageData[key];
       }
-    }
+    });
   } catch (error) {
-    console.log(error);
+    console.error('Error loading data from localStorage:', error);
   }
 })();
 
 const onFormFieldInput = event => {
   const { value, name } = event.target;
 
-  formData[name] = value.trim();
-
-  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  if (name in formData) {
+    formData[name] = value.trim();
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  }
 };
 
 const onFormFieldSubmit = event => {
   event.preventDefault();
 
+  // Перевірка, чи всі поля заповнені
   const isEmptyFields = Object.values(formData).some(value => value === '');
   if (isEmptyFields) {
     alert('Please, fill in all fields');
-  } else {
-    console.log(formData);
+    return;
   }
 
-  feedbackFormEl.reset();
-  localStorage.removeItem('feedback-form-state');
+  // Виведення даних у консоль
+  console.log(formData);
 
+  // Очищення локального сховища, форми та об'єкта formData
+  feedbackFormEl.reset();
+  localStorage.removeItem(STORAGE_KEY);
   formData = { email: '', message: '' };
 };
 
